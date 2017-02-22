05112a314927f668f12791a42b49ec2c30d76c75
Xavier Damman <xdamman@gmail.com>
2017-02-22 02:05:24 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # opencollective-postinstall
-Prompt your users to donate to your collective after `nom install`
+Prompt your users to donate to your collective after `npm install`
+
+![](https://cl.ly/323M3x2Y1J3E/Screen%20Shot%202017-02-22%20at%202.03.58%20AM.png)
+
+## Install
+
+    npm install opencollective-postinstall --save
+
+In your `package.json`, add:
+
+    {
+      ...
+      "collective": {
+        "type": "opencollective",
+        "url": "https://opencollective.com/yourcollective_slug" // e.g. https://opencollective.com/webpack
+      },
+      "scripts": {
+        "postinstall": "./bin/opencollective-postinstall"
+      },
+      ...
+    }
+
+## Suggest a donation amount
+
+In the "collective" section of your `package.json`, add: 
+
+    {
+      ...
+      "collective": {
+        "type": "opencollective",
+        "url": "https://opencollective.com/yourcollective_slug", // e.g. https://opencollective.com/webpack
+	"suggested_donation": {
+          "amount": 2,
+          "currency": "USD", // must be same currency than your collective's default currency
+          "interval": "monthly" // or "one-time" or "yearly"
+        }
+      }
+    }
+
+## Questions? Comments? Feedback?
+
+Join the #opensource channel on our slack: https://slack.opencollective.org
+
---
+++ bin/opencollective-postinstall.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const chalk = require('chalk');
+const { padding, print } = require('../lib/utils');
+const logofilepath = './src/assets/logo.txt';
+
+const logo = fs.readFileSync('./src/assets/logo.txt','utf8');
+const {
+  npm_package_name,
+  npm_package_collective_url,
+  npm_package_collective_suggested_donation_amount,
+  npm_package_collective_suggested_donation_interval,
+  npm_lifecycle_event,
+  npm_config_user_agent
+} = process.env;
+
+if (npm_lifecycle_event !== 'postinstall') {
+  console.error(`This script should be run as a postinstall script. Please add it to your package.json.`);
+  console.log(`e.g.:`);
+  console.log(padding(4), `{`);
+  console.log(padding(6), `"scripts": {`);
+  console.log(padding(8), `"postinstall": "./bin/opencollective-postinstall"`);
+  console.log(padding(6), `}`);
+  console.log(padding(4), `}`);
+  return process.exit(0);
+}
+
+if (!npm_package_collective_url) {
+  console.info(`There is no "${chalk.bold("collective")}" defined at the root of ${npm_package_name}/package.json.`);
+  console.info(`Add this variable with the url of your collective to invite users to donate to your collective.`);
+  console.info(`e.g.:`);
+  console.info(padding(4), `{`);
+  console.info(padding(6), `"name": "${npm_package_name}",`);
+  console.info(padding(6), `"collective": {`);
+  console.info(padding(8), `"type": "opencollective",`);
+  console.info(padding(8), `"url": "${chalk.underline(`https://opencollective.com/${npm_package_name}`)}"`);
+  console.info(padding(6), `}`);
+  console.info(padding(6), `... `);
+  console.info(padding(4), `}`);
+  console.info(' ');
+  return process.exit(0);
+}
+
+let donate_url = npm_package_collective_url;
+if (npm_package_collective_suggested_donation_amount) {
+  donate_url += `/${npm_package_collective_suggested_donation_amount}`;
+  if (npm_package_collective_suggested_donation_interval) {
+    donate_url += `/${npm_package_collective_suggested_donation_interval}`;
+  }
+  donate_url += (npm_config_user_agent.match(/yarn/)) ? '/yarn' : '/npm';
+}
+
+function footer() {
+  console.log("");
+  print(`Thanks for installing ${process.env.npm_package_name} 🙏.`, { color: 'yellow' });
+  print(`Please consider donating to our open collective`, { color: 'dim' });
+  print(`to help us maintain this package.`, { color: 'dim' });
+  console.log("");
+  print(`${chalk.bold("👉  Donate:")} ${chalk.underline(donate_url)}`);
+  console.log("");
+}
+
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream(logofilepath)
+});
+
+console.log("");
+lineReader.on('line', function (line) {
+  print(line, { color: 'blue'} );
+});
+
+lineReader.on('close', footer);
