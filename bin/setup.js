4397f3e3f01f22475ff130e753be8a01d1c9faab
Xavier Damman <xdamman@gmail.com>
2017-03-27 14:09:07 -0400

    Adding backers and sponsors to README.md

--- bin/setup.js
+++ bin/setup.js
@@ -29,6 +29,7 @@ const fetchData = require('../lib/fetchData');
 const print = require('../lib/print');
 
 const fetchLogo = fetchData.fetchLogo;
+const fetchBanner = fetchData.fetchBanner;
 const printLogo = print.printLogo;
 
 const parentDir = process.cwd().split('/').slice(-2, -1)[0];
@@ -40,6 +41,8 @@ if (parentDir !== 'node_modules') {
 }
 
 const projectPackageJSON = '../../package.json';
+const projectREADME = '../../README.md';
+
 var package;
 try {
   package = JSON.parse(fs.readFileSync(projectPackageJSON, 'utf8'));
@@ -98,26 +101,81 @@ const askQuestions = function() {
   console.log("You don't have any collective set in your package.json");
   console.log("Let's fix this, shall we?");
   console.log("");
-  inquirer.prompt(questions).then(answers => {
-    console.log(`Updating ${projectPackageJSON}...`);
-    package.collective = {
-      type: "opencollective",
-      url: `https://opencollective.com/${answers.collectiveSlug}`
+  return inquirer.prompt(questions).catch(e => {
+    debug("Error while running the prompt", e);
+    process.exit(0);
+  });
+}
+
+const ProcessAnswers = function(answers) {
+  console.log(`> Updating your package.json`);
+  package.collective = {
+    type: "opencollective",
+    url: `https://opencollective.com/${answers.collectiveSlug}`
+  }
+  const logo = answers.logo || answers.showLogo;
+  if (logo) {
+    package.collective.logo = logo;
+  } else {
+    delete package.collective.logo;
+  }
+  var postinstall = "./node_modules/.bin/opencollective-postinstall || exit 0";
+  if (package.scripts.postinstall && package.scripts.postinstall.indexOf(postinstall) === -1) {
+    package.scripts.postinstall = `${package.scripts.postinstall} && ${postinstall}`;
+  } else {
+    package.scripts.postinstall = postinstall;
+  }
+  fs.writeFileSync(projectPackageJSON, JSON.stringify(package, null, 2), 'utf8');
+  return updateREADME(answers.collectiveSlug);
+}
+
+const updateREADME = function(collectiveSlug) {
+  const badgesmd = `[![Backers on Open Collective](https://opencollective.com/${collectiveSlug}/backers/badge.svg)](#backers)
+[![Sponsors on Open Collective](https://opencollective.com/${collectiveSlug}/sponsors/badge.svg)](#sponsors)`;
+  const badgeshtml = `<a href="#backers" alt="sponsors on Open Collective"><img src="https://opencollective.com/${collectiveSlug}/backers/badge.svg" /></a>
+<a href="#sponsors" alt="Sponsors on Open Collective"><img src="https://opencollective.com/${collectiveSlug}/sponsors/badge.svg" /></a>`;
+
+  let readme;
+  try {
+    readme = fs.readFileSync(projectREADME, 'utf8');
+
+    if (readme.indexOf(`https://opencollective.com/${collectiveSlug}/backers/badge.svg`) !== -1) {
+      console.log("Looks like you already have Open Collective added to your README.md, skipping this step.")
+      return;
     }
-    const logo = answers.logo || answers.showLogo;
-    if (logo) {
-      package.collective.logo = logo;
-    } else {
-      delete package.collective.logo;
-    }
-    var postinstall = "./node_modules/.bin/opencollective-postinstall || exit 0";
-    if (package.scripts.postinstall && package.scripts.postinstall.indexOf(postinstall) === -1) {
-      package.scripts.postinstall = `${package.scripts.postinstall} && ${postinstall}`;
-    } else {
-      package.scripts.postinstall = postinstall;
-    }
-    fs.writeFileSync(projectPackageJSON, JSON.stringify(package, null, 2), 'utf8');
+
+    const lines = readme.split('\n');
+    const newLines = [];
+
+    let firstBadgeDetected = false;
+    lines.forEach(line => {
+      if (!firstBadgeDetected && (line.match(/badge.svg/) || line.match(/img.shields.io/))) {
+        firstBadgeDetected = true;
+        newLines.push(line.match(/<img src/) ? badgeshtml : badgesmd);
+      }
+      newLines.push(line);
+    })
+
+    return fetchBanner(collectiveSlug).then((banner) => {
+      newLines.push(banner);
+      console.log("> Adding badges and placeholders for backers and sponsors on your README.md");
+      return fs.writeFileSync(projectREADME, newLines.join('\n'), 'utf8');
+    });
+  } catch(e) {
+    console.log("> Unable to open your README.md file");
+    return;
+  }
+}
+
+console.log("");
+fetchLogo("https://opencollective.com/opencollective/logo.txt")
+  .then(printLogo)
+  .then(askQuestions)
+  .then(ProcessAnswers)
+  .then(() => {
     console.log("Done.");
+    console.log("");
+    console.log("Please double check your new updated README.md to make sure everything looks 👌.");
     console.log("");
     console.log("Protip: You can also suggest a donation amount.");
     console.log("See the docs for more options: https://github.com/opencollective/opencollective-postinstall");
@@ -125,16 +183,6 @@ const askQuestions = function() {
     console.log("Have a great day!");
     return process.exit(0);
   })
-  .catch(e => {
-    debug("Error while running the prompt", e);
-    process.exit(0);
-  });
-}
-
-console.log("");
-fetchLogo("https://opencollective.com/opencollective/logo.txt")
-  .then(printLogo)
-  .then(askQuestions)
   .catch(e => {
     debug("Error while trying to fetch the open collective logo or running the prompt", e);
     process.exit(0)
