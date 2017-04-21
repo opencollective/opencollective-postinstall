bb8caff07eb0227c7daa4810a5d66f6e09a391e0
Xavier Damman <xdamman@gmail.com>
2017-04-21 02:40:36 -0400

    using as dependency

--- test/setup.test.js
+++ test/setup.test.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 
 const paths = {
   'package': path.resolve('test/package'),
+  'postinstallpackage': path.resolve('test/package/node_modules/opencollective-postinstall'),
   'packagejson': path.resolve('test/package/package.json'),
   'parentpackage': path.resolve('test/parentpackage'),
   'parentpackagejson': path.resolve('test/parentpackage/package.json'),
@@ -31,28 +32,29 @@ describe("setup.test.js", () => {
 
   it("run setup and add postinstall script and collective info to package.json", function(done) {
     this.timeout(10000);
-    const proc = execSync("cross-env OC_POSTINSTALL_TEST=true npm install --save-dev " + path.resolve(__dirname, "../"), { cwd: paths.package });
+    const proc = execSync("npm install --save " + path.resolve(__dirname, "../"), { cwd: paths.package });
+    const setup = execSync("cross-env OC_POSTINSTALL_TEST=true npm run setup", { cwd: paths.postinstallpackage });
     const package = JSON.parse(fs.readFileSync(paths.packagejson, 'utf8'));
     expect(package.collective).to.exist;
     expect(package.scripts.postinstall).to.equal("opencollective-postinstall || exit 0");
     expect(package.collective.type).to.equal("opencollective");
     expect(package.collective.url).to.equal("https://opencollective.com/testpackage");
     expect(package.collective.logo).to.equal("https://opencollective.com/opencollective/logo.txt");
-    expect(package.devDependencies).to.have.property("opencollective-postinstall");
+    expect(package.dependencies).to.have.property("opencollective-postinstall");
     done();
   })
 
   it("runs the postinstall script after npm install", function(done) {
-    console.log(">>> cd " + paths.package + "; npm install");
+    this.timeout(10000);
     const proc = execSync("npm install", { cwd: paths.package });
     const stdout = proc.toString('utf8');
-    console.log("stdout:", stdout);
     expect(stdout).to.contain("*** Thank you for using testpackage! ***");
     expect(stdout).to.contain("https://opencollective.com/testpackage/donate");
     done();
   });
 
   it("installs a package that has opencollective-postinstall", function(done) {
+    this.timeout(10000);
     console.log(">>> cd " + paths.parentpackage + "; npm install --save " + path.resolve(__dirname, "package"));
     const proc = execSync("cross-env DEBUG=postinstall npm install --save " + path.resolve(__dirname, "package"), { cwd: paths.parentpackage });
     const stdout = proc.toString('utf8');
