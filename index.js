d76cc0aed01000abf01243c971823c2570432059
Eddie Monge <eddie.monge@consejosano.com>
2018-10-11 13:09:12 -0700

    disable showing based on npm loglevel
    - Do not show if npm's loglevel is set to warn, error or silent
    - Update Readme with instructions
    - Add Jest testing

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
-if (process.env.DISABLE_OPENCOLLECTIVE && process.env.DISABLE_OPENCOLLECTIVE !== '0' && process.env.DISABLE_OPENCOLLECTIVE.toLowerCase() !== 'false') {
-  return;
-}
-var pkg = require(require('path').resolve('./package.json'));
-if (pkg.collective) {
-  console.log(`\u001b[96m\u001b[1mThank you for using ${pkg.name}!\u001b[96m\u001b[1m`);
-  console.log(`\u001b[0m\u001b[96mIf you rely on this package, please consider supporting our open collective:\u001b[22m\u001b[39m`);
-  console.log(`> \u001b[94m${pkg.collective.url}/donate\u001b[0m\n`);
-}
+var envDisable = Boolean(process.env.DISABLE_OPENCOLLECTIVE);
+var logLevel = process.env.npm_config_loglevel;
+var logLevelDisplay = ['silent', 'error', 'warn'].indexOf(logLevel) > -1;
 
+if (!envDisable && !logLevelDisplay) {
+  var pkg = require(require('path').resolve('./package.json'));
+  if (pkg.collective) {
+    console.log(`\u001b[96m\u001b[1mThank you for using ${pkg.name}!\u001b[96m\u001b[1m`);
+    console.log(`\u001b[0m\u001b[96mIf you rely on this package, please consider supporting our open collective:\u001b[22m\u001b[39m`);
+    console.log(`> \u001b[94m${pkg.collective.url}/donate\u001b[0m\n`);
+  }
+}
